439145ca3082b2ae1cede29306916a04a9adcd4c
Your Name <you@example.com>
2018-05-16 04:08:52 -0300

    merge dos dois codigos

--- ray2.c
+++ ray2.c
@@ -3,8 +3,6 @@
 #include <stdio.h>
 #include <math.h>
 
-#define min(a,b) (((a) < (b)) ? (a) : (b))
-
 typedef struct vector{
   double x, y, z;
 } vector;
@@ -19,14 +17,14 @@ typedef struct sphere{
   int material;
 } sphere;
 
-typedef struct colour {
-  double red, green, blue;
-}colour;
 typedef struct material{
 	colour diffuse;
 	float reflection;
 }material;
 
+typedef struct colour {
+  double red, green, blue;
+}colour;
 
 typedef struct light{
   vector pos;
@@ -112,10 +110,9 @@ void add_ray(ray *r, double o_x, double o_y, double o_z, double d_x, double d_y,
 
 
 
-void add_sphere(sphere *s, double x, double y, double z, double r, int m){
+void add_sphere(sphere *s, double x, double y, double z, double r){
   add_vec(&(s->c), x, y, z);
   s->r = r;
-  s->material = m;
 }
 
 vector * get_normal_sphere(sphere *s, vector *pi) {
@@ -150,9 +147,9 @@ int intersect(sphere *s, ray *r, double *t) {
 
   if((t0 > 0.001f) && (t0 < *t)){
     *t = t0;
-    return 1;
+    retval = 1;
   }else
-    return 0;
+    retval = 0;
 }
 
 void clamp255(vector *v) {
@@ -180,9 +177,10 @@ void saveppm(char *filename, unsigned char *img, int width, int height){
 
 int main() {
 
-  const int HEIGHT = 600;
-  const int WIDTH = 800;
+  const int HEIGHT = 500;
+  const int WIDTH = 500;
   unsigned char img[3*WIDTH*HEIGHT];
+  int hit;
 
   // vector *white;
   // white = malloc(sizeof(vector));
@@ -196,64 +194,22 @@ int main() {
   // red = malloc(sizeof(vector));
   // add_vec(red, 255, 0, 0);
 
-  printf("oi");
-  sphere spheres[3];
-  add_sphere(&spheres[0], 200, 300, 50, 50, 0);
-  add_sphere(&spheres[1], WIDTH*0.1, HEIGHT*0.1, 50, 50, 1);
-  add_sphere(&spheres[2], WIDTH*0.8, HEIGHT*0.8, 50, 50, 2);
-
-  material materials[3];
-	materials[0].diffuse.red = 1;
-	materials[0].diffuse.green = 0;
-	materials[0].diffuse.blue = 0;
-	materials[0].reflection = 0.2;
-
-	materials[1].diffuse.red = 0;
-	materials[1].diffuse.green = 1;
-	materials[1].diffuse.blue = 0;
-	materials[1].reflection = 0.5;
-
-	materials[2].diffuse.red = 0;
-	materials[2].diffuse.green = 0;
-	materials[2].diffuse.blue = 1;
-	materials[2].reflection = 0.9;
-
-  light lights[3];
-
-	lights[0].pos.x = 0;
-	lights[0].pos.y = 240;
-	lights[0].pos.z = -100;
-	lights[0].intensity.red = 1;
-	lights[0].intensity.green = 1;
-	lights[0].intensity.blue = 1;
-
-	lights[1].pos.x = 3200;
-	lights[1].pos.y = 3000;
-	lights[1].pos.z = -1000;
-	lights[1].intensity.red = 0.6;
-	lights[1].intensity.green = 0.7;
-	lights[1].intensity.blue = 1;
-
-	lights[2].pos.x = 600;
-	lights[2].pos.y = 0;
-	lights[2].pos.z = -100;
-	lights[2].intensity.red = 0.3;
-	lights[2].intensity.green = 0.5;
-	lights[2].intensity.blue = 1;
+  sphere s;
+  add_sphere(&s, WIDTH*0.5, HEIGHT*0.5, 50, 50);
 
   // sphere light;
   // add_sphere(&light, 0, 0, 50, 1);
 
   // vector *pix_col;
   // pix_col = malloc(sizeof(vector));
+  colour *rgb;
+  rgb = malloc(sizeof(colour));
   ray *r;
   r = malloc(sizeof(ray));
   for (int y = 0; y < HEIGHT; ++y) {
     for (int x = 0; x < WIDTH; ++x) {
       // copy_vec(black, pix_col);
-      double red = 0;
-      double green = 0;
-      double blue = 0;
+      add_colour(rgb, 0, 0, 0);
       int level = 0;
       double coef = 1.0;
 
@@ -264,70 +220,28 @@ int main() {
         int currentSphere = -1;
         //procura a esfera mais proxima desse pixel
         for(int i = 0; i < 3; i++){
-          if(intersect(&spheres[i], r, &t))
+          if(intersect(&s, r, &t))
             currentSphere = i;
         }
         if (currentSphere == -1) break;
 
-        //criando o ray atual do pixel
-        vector *pi;
-        vector *aux;
-        aux = mult_vec(&(r->d), t);
-        pi = sum_vec(&(r->o), aux);
 
-        /* Find the normal for this new vector at the point of intersection */
-        vector *L;
-        L = sub_vec(&(spheres[currentSphere].c), pi);
-        vector *N;
-        N = get_normal_sphere(&spheres[currentSphere], pi);
-        normalize(L);
-        normalize(N);
-        double dt = dot(L, N);
-        //
-        material currentMat = materials[spheres[currentSphere].material];
 
-        //calcula o valor da luz nesse pixel
-        for(int j = 0; j < 3; j++){
-          light currentLight = lights[j];
-          vector *dist;
-          dist = sub_vec(&currentLight.pos, pi);
-					if(dot(N, dist) <= 0.0f) continue;
-					float t = sqrtf(dot(dist,dist));
-					if(t <= 0.0f) continue;
-
-					vector *lightRay;
-					lightRay = mult_vec(dist, (1/t));
-          /* Lambert diffusion */
-          double lambert = dot(lightRay, N) * coef;
-          // float lambert = 0.0;
-          red += lambert * currentLight.intensity.red * currentMat.diffuse.red;
-          green += lambert * currentLight.intensity.green * currentMat.diffuse.green;
-          blue += lambert * currentLight.intensity.blue * currentMat.diffuse.blue;
-        }
-
-        /* Iterate over the reflection */
-        coef *= currentMat.reflection;
-
-        /* The reflected ray start and direction */
-        r->o = *pi;
-        float reflect = 2.0f * dot(&r->d, N);
-        vector *tmp;
-        tmp = mult_vec(N, reflect);
-        r->d = *sub_vec(&r->d, tmp);
-
-        level++;
-
-      }while((coef > 0.0f) && (level < 15));
+      }while
 
 
 
-// printf("oi");
 
-      img[(x + y*WIDTH)*3 + 0] = (unsigned char)min(red*255.0f, 255.0f);
-      img[(x + y*WIDTH)*3 + 1] = (unsigned char)min(green*255.0f, 255.0f);
-      img[(x + y*WIDTH)*3 + 2] = (unsigned char)min(blue*255.0f, 255.0f);
-
-
+      hit = intersect(&s, r, &t);
+      if(hit) {
+        img[(x + y*WIDTH)*3 + 0] = 255;
+				img[(x + y*WIDTH)*3 + 1] = 0;
+				img[(x + y*WIDTH)*3 + 2] = 0;
+			}else{
+				img[(x + y*WIDTH)*3 + 0] = 0;
+				img[(x + y*WIDTH)*3 + 1] = 0;
+				img[(x + y*WIDTH)*3 + 2] = 0;
+			}
       // if (intersect(&s, r, &t)) {
       //   vector *pi;
       //   vector *aux;
@@ -360,8 +274,6 @@ int main() {
       // }
       // fprintf(fp, "%d %d %d\n", (int)pix_col->x, (int)pix_col->y, (int)pix_col->z);
     }
-
   }
-
   saveppm("image.ppm", img, WIDTH, HEIGHT);
 }
